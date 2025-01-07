fb6dd1e21ab963615068dd84c1e115abb1b4f9aa
Salman <bhattiashfaq068@gmail.com>
2025-01-07 10:32:11 +0500

    Start button activation

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import './style.css'
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+const exitBtn = document.querySelector('.exit-btn');
+const continueBtn = document.querySelector('.continue-btn');
+const mainDiv = document.querySelector('.main')
+
+startBtn.addEventListener('click', ()=>{
+    popupInfo.classList.add('active')
+    mainDiv.classList.add('active')
+})
+
+exitBtn.addEventListener('click', ()=>{
+    popupInfo.classList.remove('active')
+    mainDiv.classList.remove('active')
+})
 
