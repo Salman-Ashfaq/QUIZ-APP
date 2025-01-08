4d4fdb5aa6d129c36520e6ca957b9ab0844e2e55
Salman <bhattiashfaq068@gmail.com>
2025-01-08 16:50:50 +0500

    updating the questions using next btn

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import './style.css'
+import { questions } from './questions.js';
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const continueBtn = document.querySelector('.continue-btn');
 const mainDiv = document.querySelector('.main')
+const quizInfo = document.querySelector('.quiz-info')
+const quizSec = document.querySelector('.quiz-section')
 
 startBtn.addEventListener('click', ()=>{
     popupInfo.classList.add('active')
@@ -14,4 +17,28 @@ exitBtn.addEventListener('click', ()=>{
     popupInfo.classList.remove('active')
     mainDiv.classList.remove('active')
 })
+
+continueBtn.addEventListener('click', ()=>{
+    quizInfo.classList.add('active')
+    quizSec.classList.add('active')
+    popupInfo.classList.remove('active')
+    mainDiv.classList.remove('active')
+    showQues(0)
+})
+
+let quesCount = 0;
+
+const nextBtn = document.querySelector('.next-ques')
+
+nextBtn.addEventListener('click', ()=>{
+    quesCount++
+    showQues(quesCount)
+})
+
+
+function showQues (index) {
+  const quesText=document.querySelector('.ques-statment')
+  quesText.innerText = `${questions[index].numb}. ${questions[index].statment}`
+}
+
 
---
+++ src/questions.js
@@ -0,0 +1,61 @@
+export const questions = [
+    {
+        numb : 1 ,
+        statment : 'What is the correct syntax to declare a variable in JavaScript?' ,
+        answer :'D. All of the above' ,
+        options : [
+            'A. let variableName' ,
+            'B. var variableName' ,
+            'C. const variableName' ,
+            'D. All of the above' 
+        ]
+    }
+    ,
+    {
+        numb : 2 ,
+        statment : 'Which method is used to find the length of a string in JavaScript?' ,
+        answer :'C. str.length' ,
+        options : [
+            'A. length()' ,
+            'B. size()' ,
+            'C. str.length' ,
+            'D. strlen()' 
+        ]
+    }
+    ,
+    {
+        numb : 3 ,
+        statment : 'Which of the following is NOT a JavaScript data type?' ,
+        answer :' D. Character' ,
+        options : [
+            'A. String' ,
+            'B. Boolean' ,
+            'C. Undefined' ,
+            'D. Character' 
+        ]
+    }
+    ,
+    {
+        numb : 4 ,
+        statment : 'What does the === operator do in JavaScript?' ,
+        answer :'C. Checks for both value and type equality' ,
+        options : [
+            'A. Checks for value equality only' ,
+            'B. Checks for type equality only' ,
+            'C. Checks for both value and type equality' ,
+            'D. None of the above' 
+        ]
+    }
+    ,
+    {
+        numb : 5 ,
+        statment : 'Which of the following is the correct way to create a function in JavaScript?' ,
+        answer :' A. function myFunction() { }' ,
+        options : [
+            'A. function myFunction() { }' ,
+            'B. def myFunction() { }' ,
+            'C. create myFunction() { }' ,
+            'D. method myFunction() { }' 
+        ]
+    }
+]
